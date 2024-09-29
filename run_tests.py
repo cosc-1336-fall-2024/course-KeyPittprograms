aaf25bab90b57cadd90819239cac49342127cb7d
KeyPittprograms <keshawn.pittman@g.austincc.edu>
2024-09-28 23:59:53 -0500

    Homework 4

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,4 @@
+#Replace tests_repetion with tests_functions.
 import unittest
-'''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
-has the test functions
-'''
-from tests.homework.b_in_proc_out import tests_in_proc_out
-#from tests.examples.b_input_process_output import tests_input_process_output
-
-suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
-unittest.TextTestRunner(verbosity=2).run(suite)
+from tests.homework.e_functions import tests_functions
+suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,14 @@
-#
+# Create a function named get_factorial
+# Parameter: Some number N
+# Returns the factorial of number N
+
+def get_factorial(number):
+    product = int(number)
+    for n in range(number):
+        if ((number - n) > 2):
+            start_value = product
+            product *= (number - n - 1)
+            print(n, n-1, start_value, product)
+    return product
+
+print(get_factorial(5))
